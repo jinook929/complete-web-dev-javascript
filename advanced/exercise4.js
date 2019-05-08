f45b7a3b409424ae70482ae1775294b2d9bdc382
jinook929 <jinook929@gmail.com>
2019-05-07 23:09:17 -0400

    closure, currying & compose exercise

--- advanced/exercise4.js
+++ advanced/exercise4.js
@@ -31,31 +31,52 @@ const sum = (num) => num + 1;
 
 compose(sum, sum)(5);
 
+//===========================================================
 //Solve these problems:
 
-//#1 Create a one line function that adds adds two parameters
+//#1 Create a one line function that adds two parameters
+const addTwoNum = (a, b) => a + b;
 
 //Closure: What does the last line return?
 const addTo = x => y => x + y
 var addToTen = addTo(10)
-addToTen(3)
+addToTen(3) // answer: 13
+
+function addTo_(x) {
+  return function(y) {
+    return x + y;
+  }
+}
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
-curriedSum(30)(1)
+curriedSum(30)(1) // answer: 31
 
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
 const add5 = curriedSum(5)
-add5(12)
+add5(12) // answer: 17
 
 //Composing: What does the last line return?
 const compose = (f, g) => (a) => f(g(a));
 const add1 = (num) => num + 1;
 const add5 = (num) => num + 5;
-compose(add1, add5)(10)
+compose(add1, add5)(10) // answer: 16
+
+function add1(num) {
+  return num + 1;
+}
+
+function add5(num) {
+  return num + 5;
+}
+
+let g = add5(10); // output = 15
+let f = add1(15); // output = 16
 
 //What are the two elements of a pure function?
+// 1. Deterministic --> always produces the same results given the same inputs
+// 2. No Side Effects -->  It does not depend on any state, or data, change during a program’s execution. It must only depend on its input elements.
